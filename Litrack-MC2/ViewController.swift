4c2d485dd0e2396432613c4377b52bcce37ddc1f
Karina Widyastuti <karinawidyastuti7@gmail.com>
2020-05-20 00:49:19 +0700

    add x-code with POD installed

---
+++ Litrack-MC2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Litrack-MC2
+//
+//  Created by Karina Widyastuti on 20/05/20.
+//  Copyright © 2020 Karina Widyastuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
