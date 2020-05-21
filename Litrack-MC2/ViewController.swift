74cb49e350ccc8a274add98be88e987c776f946e
Karina Widyastuti <karinawidyastuti7@gmail.com>
2020-05-22 01:39:22 +0700

    set onboarding screen

--- Litrack-MC2/ViewController.swift
+++ Litrack-MC2/ViewController.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcome: UILabel!
+    @IBOutlet weak var getStarted: UIButton!
+    
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var label3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        // setup welcome
+        self.welcome.text = "Welcome!"
+        self.welcome.font = UIFont.boldSystemFont(ofSize: 60)
+        self.welcome.textAlignment = .center
+        
+        // setup stack view
+        self.image1.image = UIImage(named: "Onboarding 1st Icon")
+        self.label1.font = UIFont.systemFont(ofSize: 21)
+        self.label1.text = "Add your waste with the camera feature"
+        
+        self.image2.image = UIImage(named: "Onboarding 2nd Icon")
+        self.label2.font = UIFont.systemFont(ofSize: 21)
+        self.label2.text = "Get data and track your waste production"
+        
+        self.image3.image = UIImage(named: "Onboarding 3rd Icon")
+        self.label3.font = UIFont.systemFont(ofSize: 21)
+        self.label3.text = "Get tips on how to reduce and recycle it"
+        
+        
+        // setup button
+        self.getStarted.setTitle("Get Started", for: .normal)
+        self.getStarted.backgroundColor = UIColor(red: 59/255, green: 126/255, blue: 115/255, alpha: 1)
+        self.getStarted.setTitleColor(.white, for: .normal)
+        self.getStarted.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+        self.getStarted.layer.cornerRadius = 10
     }
 
 
