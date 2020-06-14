d01c956a380403f169756d0a330890e1ef215b67
Karina Widyastuti <karinawidyastuti7@gmail.com>
2020-06-14 11:18:49 +0700

    Add voice over

--- Litrack-MC2/Camera/PreviewViewController.swift
+++ Litrack-MC2/Camera/PreviewViewController.swift
@@ -29,7 +29,7 @@ class PreviewViewController: UIViewController {
    }
     
     override func viewDidAppear(_ animated: Bool) {
-        let voiceOver = "You added," + name + "preview".localized
+        let voiceOver = "You add" + name + ".\n" + "preview".localized
         speechService.speaking(voiceOver)
     }
    
